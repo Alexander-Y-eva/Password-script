15a17d4140c9405df309c3d0c2a86422561c56c9
Alexander-Y-eva <cookienookie24@gmail.com>
2024-07-07 10:59:25 -0700

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,13 @@
+answer = input("What is your password? ")
+if answer == "98237":
+    print("Simple password code in python by Alexander Yzaguirre")
+    print("Welcome to the Termanal.")
+    y = input("See Youtube link? (Y/N)")
+else:
+    print("simple password code in python written by Alexander Yzaguirre")
+    print("Password incorrect please restart program.")
+if y == "Y":
+    print("https://www.youtube.com/channel/UCR0u6AVwhdJ5Zwu9HZHmGSA")
+z = input("See Github link? (Y/N)")
+if z == "Y":
+    print("https://github.com/Alexander-Y-eva")
